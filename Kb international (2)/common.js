d9d2181190cd593e7095542c17586403da0cce99
ABDULRAAFAAYY <130109622+ABDULRAAFAAYY@users.noreply.github.com>
2025-08-01 06:48:15 +0500

    Update common.js

--- Kb international (2)/common.js
+++ Kb international (2)/common.js
@@ -110,7 +110,7 @@ function displayVehicles(vehiclesToDisplay, gridSelector = '.vehicle-grid') {
                 <p><strong>Grade:</strong> ${vehicle.grade}</p>
                 <p><strong>Mileage:</strong> ${vehicle.mileage}</p>
                 <p><strong>Transmission:</strong> ${vehicle.transmission}</p>
-                <div class="vehicle-price">$${vehicle.price.toLocaleString()}</div>
+                <div class="vehicle-price">$${vehicle.price.toLocaleString()} (Prices are negotiable)</div>
                 <a href="product.html?carId=${vehicle.id}" class="btn">View Details</a>
             </div>
         `;
